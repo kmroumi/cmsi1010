129ae545b81e53fdf8f943c6fef741e28b15258a
Kmroumi <kmroumi@gmail.com>
2025-06-09 15:17:04 -0700

    finished lab09

--- lab09/hanoi.py
+++ lab09/hanoi.py
@@ -1,10 +1,14 @@
 import sys
 
+number_of_moves = 0
+
 
 def move(n, source, destination, auxiliary):
     if n > 0:
         move(n - 1, source, auxiliary, destination)
         print(f"Move disk {n} from {source} to {destination}")
+        global number_of_moves
+        number_of_moves += 1
         move(n - 1, auxiliary, destination, source)
 
 
@@ -14,9 +18,10 @@ try:
     if not sys.argv[1].isdigit():
         raise ValueError("The argument must be a positive integer")
     number_of_disks = int(sys.argv[1])
-    if number_of_disks <= 1 or number_of_disks > 20:
+    if number_of_disks < 1 or number_of_disks > 20:
         raise ValueError(
             "The argument must be between 1 and 20, inclusive")
     move(number_of_disks, "A", "B", "C")
+    print(f"\nTotal number of moves made: {number_of_moves}")
 except ValueError as e:
     print(f"Error: {e}")
