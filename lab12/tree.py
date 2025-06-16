26e743441d1f6e3728bf692b7d21ce958d667d94
Kmroumi <kmroumi@gmail.com>
2025-06-16 15:00:11 -0700

    finished lab12

--- lab12/tree.py
+++ lab12/tree.py
@@ -1,30 +1,23 @@
-from math import pi, radians as rad
+from math import radians
 from Tree.core import Tree
 from PIL import Image
 
-branches = (
-    (.5, rad(-30)),
-    (.6, rad(30)),
-    (.4, rad(60))
-)
+background_color = (200, 240, 250)
+leaf_color = (189, 26, 167)
+trunk_width = 30
+base_trunk_color = (50, 40, 0)
+small_branch_color = (180, 130, 30)
+branch_gradient = (*base_trunk_color, *small_branch_color)
 
+trunk_length = 200
+first_branch_line = (0, 0, 0, -trunk_length)
+scales_and_angles = [(0.7, radians(-30)), (0.6, radians(30))]
+age = 8
 
-def main():
-    tree = Tree(
-        pos=(0, 0, 0, -500),
-        branches=branches
-    )
-
-    # Let the tree grow
-    tree.grow(10)
-
-    # Move the tree in the right position, so that the tree is completly in the image
-    tree.move_in_rectangle()
-
-    im = Image.new("RGB", tree.get_size(), (239, 239, 239))
-    tree.draw_on(im, (85, 25, 0, 128, 53, 21), (0, 62, 21), 10)
-    im.show()
-
-
-if __name__ == '__main__':
-    main()
+tree = Tree(pos=first_branch_line,
+            branches=scales_and_angles, sigma=(0.1, 0.2))
+tree.grow(age)
+tree.move_in_rectangle()
+image = Image.new("RGB", tree.get_size(), background_color)
+tree.draw_on(image, branch_gradient, leaf_color, trunk_width)
+image.show()
