d031d14ce58fa7a8400872f6c1d0d2baf1024a63
Kmroumi <kmroumi@gmail.com>
2025-06-18 14:03:41 -0700

    finished lab14

--- lab14/ufo.py
+++ lab14/ufo.py
@@ -1,4 +1,6 @@
 import pygame
+from dataclasses import dataclass
+
 
 WIDTH, HEIGHT = 800, 600
 SKY_COLOR = (0, 255, 255)
@@ -15,16 +17,48 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Alien Invasion")
 
 
+@dataclass
+class UFO:
+    x: int
+    y: int
+    width: int = 100
+    height: int = 30
+    color: tuple = (128, 128, 128)
+    speed: int = 1
+
+    def draw(self):
+        pygame.draw.ellipse(
+            screen, self.color, (self.x, self.y, self.width, self.height))
+        pygame.draw.ellipse(
+            screen, self.color,
+            (self.x + self.width//4, self.y-self.height//3, self.width//2, self.height))
+
+    def move(self):
+        self.x += self.speed
+        if self.x > WIDTH:
+            self.x = -self.width
+
+
+ufos = [
+    UFO(x=0, y=50),
+    UFO(x=200, y=100, speed=3.5, width=80, height=20),
+    UFO(x=400, y=150, color=(160, 160, 160), width=120, speed=3),
+    UFO(x=600, y=200, speed=4)]
+
+
 def draw_scene():
     screen.fill(SKY_COLOR)
     pygame.draw.circle(screen, SUN_COLOR, SUN_POSITION, SUN_RADIUS)
     pygame.draw.rect(screen, GRASS_COLOR, GRASS_RECTANGLE)
+    for ufo in ufos:
+        ufo.draw()
+        ufo.move()
     pygame.display.flip()
 
 
-draw_scene()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.display.quit()
             raise SystemExit
+    draw_scene()
